8a77bac5881760381901bde34dd1c386179ae761
AlddnAy <alaaddin.ay@ogr.sakarya.edu.tr>
2024-07-18 00:01:59 +0300

    First Commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,88 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { 
+  Image, 
+  StyleSheet,
+  Platform,
+  Text,
+  View ,
+  TextInput,
+  Pressable,
+} from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React,{useState} from 'react';
+
 
 export default function HomeScreen() {
+  
+  const [name,setName]=useState("")
+  const [lastname,setLastName]=useState("")
+  const [result,setResult]=useState("")
+  
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View style ={styles.container}>
+
+      <Text>Welcome {result}</Text>
+       <TextInput></TextInput>
+
+
+      <Text>Name</Text>
+      <TextInput 
+      placeholder='Enter Your Name'
+      style={styles.textInputStyle}
+      onChangeText={(value)=>setName(value)}
+      />
+      <Text>Last Name</Text>
+      <TextInput
+       placeholder='Enter Your Last Name'
+       style={styles.textInputStyle}
+       onChangeText={(value)=>setLastName(value)}
+       />
+
+       <Pressable 
+       onPress={()=>setResult(name+" "+lastname)}
+       style={({pressed})=>[{
+        
+        backgroundColor: pressed ? "gray" : "lightblue"},
+        
+        styles.button]   }>
+      <Text style={styles.buttonText}>Save</Text>
+
+       </Pressable>
+
+    </View>
+
+);
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex:1,
+    backgroundColor: "#fff",
+    alignItems:"center",
+    justifyContent:"center"
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  textInputStyle:{
+    borderWidth:1,
+    width:"80%",
+    height:50,
+    borderRadius:10,
+    marginVertical:10,
+    textAlign:"center",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  button:{
+    borderWidth:1,
+    width:"80%",
+    height:50,
+    borderRadius:10,
+    alignItems:"center",
+    justifyContent:"center",
+    
+
+    
   },
+  buttonText:{
+    fontWeight:"bold",
+    color:"White"
+  }
+  
+
 });
